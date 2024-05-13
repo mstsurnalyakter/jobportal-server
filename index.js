af032757bab5f3559c65a87723e6bc6e9c46795f
mstsurnalyakter <surnalyms@gmail.com>
2024-05-13 11:39:32 +0600

    applied-job api added

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-     "https://job-portal-f2a64.web.app"
-    ],
+  origin: ["http://localhost:5173", "https://job-portal-f2a64.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -18,12 +15,8 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
-
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -35,69 +28,103 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     const jobCollection = client.db("jobPortal").collection("jobs");
     const applyJobCollection = client.db("jobPortal").collection("applyJobs");
 
     //job related api
-    app.get("/jobs", async(req,res)=>{
-        const result = await jobCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/jobs", async (req, res) => {
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get("/all-jobs",async(req,res)=>{
-        const page = parseInt(req.query.page) - 1;
-        const size = parseInt(req.query.size);
-        const search = req.query.search;
+    app.get("/all-jobs", async (req, res) => {
+      const page = parseInt(req.query.page) - 1;
+      const size = parseInt(req.query.size);
+      const search = req.query.search;
 
-        const query = {
-          jobTitle: { $regex: search, $options: "i" },
-        };
-        const result = await jobCollection
+      const query = {
+        jobTitle: { $regex: search, $options: "i" },
+      };
+      const result = await jobCollection
         .find(query)
-        .skip(page*size)
+        .skip(page * size)
         .limit(size)
         .toArray();
-        res.send(result);
+      res.send(result);
+    });
 
-    })
+    app.get("/jobs-count", async (req, res) => {
+      const search = req.query.search;
+      let query = {
+        jobTitle: { $regex: search, $options: "i" },
+      };
+      const count = await jobCollection.countDocuments(query);
+      res.send({ count });
+    });
 
-
-    app.get("/jobs-count", async(req,res)=>{
-        const search = req.query.search;
-        let query = {
-          jobTitle: { $regex: search, $options: "i" },
-        };
-        const count = await jobCollection.countDocuments(query);
-        res.send({count})
-
-    })
-
-    app.post("/add-jobs",async(req,res)=>{
+    app.post("/add-jobs", async (req, res) => {
       const result = await jobCollection.insertOne(req.body);
       res.send(result);
-    })
-
-    app.post("/apply-job",async(req,res)=>{
-      console.log(req.body);
-      const result = await applyJobCollection.insertOne(req.body);
-      res.send(result);
     });
 
-    app.get("/my-jobs/:email", async(req,res)=>{
-      const result = await jobCollection.find({"user.email":req.params.email}).toArray();
-      res.send(result);
-    })
-    app.delete("/my-job/:id", async (req, res) => {
+    app.get("/my-jobs/:email", async (req, res) => {
       const result = await jobCollection
-        .deleteOne({ _id: new ObjectId(req.params.id) })
+        .find({ "user.email": req.params.email })
+        .toArray();
+      res.send(result);
+    });
+    app.delete("/my-job/:id", async (req, res) => {
+      const result = await jobCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(result);
     });
 
-    app.get("/job/:id", async(req,res)=>{
-      const result = await jobCollection.findOne({_id: new ObjectId(req.params.id)});
-      res.send(result)
-    })
+    app.get("/job/:id", async (req, res) => {
+      const result = await jobCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    app.post("/apply-job", async (req, res) => {
+      const applyData = req.body;
+
+      const alreadyApplied = await applyJobCollection.findOne({
+        userEmail: applyData.userEmail,
+        jobId: applyData.jobId,
+      });
+
+      if (alreadyApplied) {
+        return res.status(400).send("You have already applied for this job.");
+      }
+
+      const result = await applyJobCollection.insertOne(applyData);
+
+      const updateDoc = {
+        $inc: { jobApplicantsNumber: 1 },
+      };
+      const query = { _id: new ObjectId(applyData.jobId) };
+
+      await jobCollection.updateOne(query, updateDoc);
+
+      res.send(result);
+    });
+
+    app.get("/applied-job",async(req,res)=>{
+      const filter = req.query?.filter;
+
+      const query = {
+        userEmail:req.query?.email
+      };
+
+      if (filter) query.jobCategory = filter;
+
+
+      const result = await applyJobCollection.find(query).toArray();
+      res.send(result);
+    });
+
 
 
     console.log(
@@ -109,12 +136,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("I am from Home route");
